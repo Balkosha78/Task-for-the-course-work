edc9eff1eefa72d5f0b1cae2b83e01519086d714
Алексей <balko-aleksey@yandex.ru>
2023-03-26 14:25:15 +0700

    Task completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
+    public static boolean chekedDataPalindrome (String data) {
+        int len = data.length();
+        for (int i = 0; i < len / 2; i++) {
+            int idx = len - i - 1;
+            if (data.charAt(i) != data.charAt(idx)) {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        String data = in.nextLine();
+        System.out.println(chekedDataPalindrome(data));
     }
 }
